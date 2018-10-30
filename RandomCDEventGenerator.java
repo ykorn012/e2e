2f56f291310d7fe3b1d58f0c349f0527471f18a0
크게접은하늘 <ykorn012@gmail.com>
2018-10-31 04:03:29 +0900

    Update RandomCDEventGenerator.java

--- RandomCDEventGenerator.java
+++ RandomCDEventGenerator.java
@@ -1,4 +1,4 @@
-package com.cor.cep.util;
+package com.isd.cep.util;
 
 import java.util.Date;
 import java.util.Random;
@@ -10,8 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.cor.cep.event.CDEvent;
-import com.cor.cep.handler.CDEventHandler;
+import com.isd.cep.event.CDEvent;
+import com.isd.cep.handler.CDEventHandler;
 
 /**
  * Just a simple class to create a number of Random CDEvents and pass them off to the
@@ -42,14 +42,15 @@ public class RandomCDEventGenerator {
                 int count = 1;
                 while (count < noOfCDEvents) {
                     CDEvent ve = new CDEvent(count, new Random().nextInt(500), new Date());
-                    CDEventHandler.handle(ve);
-                    count++;
                     try {
+                    	CDEventHandler.handle(ve);
+                    	count++;
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
                         LOG.error("Thread Interrupted", e);
                     }                    
                 }
+                System.exit(0);
 
             }
         });
@@ -58,11 +59,11 @@ public class RandomCDEventGenerator {
     
     private String getStartingMessage(){
         StringBuilder sb = new StringBuilder();
-        sb.append("\n\n************************************************************");
-        sb.append("\n* STARTING - ");
-        sb.append("\n* PLEASE WAIT - CDs ARE RANDOM SO MAY TAKE");
+        sb.append("\n\n************************************************************************\"");
+        sb.append("\n* STARTING - Etching Equipment #1 APC (Advanced Process Control) DEMO");
+        sb.append("\n* PLEASE WAIT - CDs ARE 100 RANDOM");
         sb.append("\n* A WHILE TO SEE WARNING AND CRITICAL EVENTS!");
-        sb.append("\n************************************************************\n");
+        sb.append("\n*******************************************\"*****************************\n");
         return sb.toString();
     }
 }
